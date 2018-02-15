8f7ab190a3b1739fe4d4944f6387359e0568e18d
Pau Enrech <pauenrech94@gmail.com>
2018-02-15 20:57:14 +0100

    Todos los botones funcionan. Añadidos diferentes mensajes de error

--- app/src/main/java/com/pauenrech/fakephone/FakePhoneActivity.java
+++ app/src/main/java/com/pauenrech/fakephone/FakePhoneActivity.java
@@ -2,6 +2,7 @@ package com.pauenrech.fakephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +11,6 @@ import android.widget.Toast;
 
 public class FakePhoneActivity extends AppCompatActivity {
 
-    private Button btn1;
     private TextView phoneText;
 
     @Override
@@ -20,13 +20,15 @@ public class FakePhoneActivity extends AppCompatActivity {
 
         phoneText = findViewById(R.id.numberText);
 
-        btn1 = findViewById(R.id.btn1);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                phoneText.append(btn1.getText());
-            }
-        });
+    }
+
+    public void BtnPress(View view) {
+        if (phoneText.length() < 10) {
+            Button numPress = findViewById(view.getId());
+            phoneText.append(numPress.getText());
+        } else {
+            Toast.makeText(this, R.string.TooLongNumber, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -36,12 +38,12 @@ public class FakePhoneActivity extends AppCompatActivity {
     }
 
     public void callClick(View view) {
-        String numberToCall =  phoneText.getText().toString();
+        String numberToCall = phoneText.getText().toString();
         String msg;
-        if (numberToCall.isEmpty()){
-            msg = "No has introduit cap digit";
+        if (numberToCall.isEmpty()) {
+            msg = getString(R.string.NoNumberMsg);
         } else {
-            msg = "Trucant al " + numberToCall;
+            msg = getString(R.string.CallingTo) + numberToCall;
         }
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
