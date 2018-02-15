e00f0863845ee24c1db8837f6a7e6cf69d3b9f69
Pau Enrech <pauenrech94@gmail.com>
2018-02-15 20:14:38 +0100

    El número 1 ya funciona

--- app/src/main/java/com/pauenrech/fakephone/FakePhoneActivity.java
+++ app/src/main/java/com/pauenrech/fakephone/FakePhoneActivity.java
@@ -2,12 +2,30 @@ package com.pauenrech.fakephone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private TextView phoneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
+
+        phoneText = findViewById(R.id.numberText);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneText.append(btn1.getText());
+            }
+        });
+
     }
 }
